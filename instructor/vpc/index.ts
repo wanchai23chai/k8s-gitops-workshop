2eb3164f3bb69ad4ff0c67da2aaa67e4ae2167e5
WhatTheFar <jakpat.m@gmail.com>
2020-12-16 00:05:46 +0700

    feat(in): init ecr for instructor

--- instructor/vpc/index.ts
+++ instructor/vpc/index.ts
@@ -1,3 +1,4 @@
+import * as aws from '@pulumi/aws';
 import * as awsx from '@pulumi/awsx';
 
 const vpc = new awsx.ec2.Vpc('k8s-workshop', {
@@ -8,5 +9,19 @@ const vpc = new awsx.ec2.Vpc('k8s-workshop', {
   },
 });
 
+const apiGateway = new aws.ecr.Repository('api-gateway', {
+  name: `k8s-gitops-workshop-api-gateway`,
+});
+
+const calculator = new aws.ecr.Repository('calculator', {
+  name: `k8s-gitops-workshop-calculator`,
+});
+
 export const vpcId = vpc.id;
 export const vpcPublicSubnetIds = vpc.publicSubnetIds;
+
+export const apiGatewayRepositoryName = apiGateway.name;
+export const apiGatewayRepositoryUrl = apiGateway.repositoryUrl;
+
+export const calculatorRepositoryName = calculator.name;
+export const calculatorRepositoryUrl = calculator.repositoryUrl;
